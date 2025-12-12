47fe72f0b214a3dc4c61655b23f162c05c1f4ea4
yycy <1171850332@qq.ocm>
2025-12-12 18:03:45 +0800

    代码提交

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
   build: {
     // 构建优化选项
     sourcemap: false, // 禁用 sourcemap 以减小文件大小
-    minify: "terser", // 使用 terser 进行代码压缩
+    // minify: "terser", // 使用 terser 进行代码压缩
   },
 });
